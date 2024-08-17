e150792578079f4b4c0012705986fbf1115968cf
souryadeepRC <thecreativeworldforall@gmail.com>
2024-08-17 14:47:16 +0530

    additional test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.scss";
 
 function App() {
   const test = "Hello world";
-  return <div className="app-container">Developer portfolio NEW</div>;
+  return <div className="app-container">Developer portfolio NEW-1</div>;
 }
 
 export default App;
