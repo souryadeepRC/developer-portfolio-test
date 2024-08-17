60fefd11d34462fe941f3d86d8e0a17441cb4943
Souryadeep Roy Chowdhury <84499348+souryadeepRC@users.noreply.github.com>
2024-08-17 15:52:47 +0530

    Test3 (#17)
    * test codacy
    
    * Update App.tsx
    
    * Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.scss";
 
 function App() {
-  const test = "Hello world";
-  return <div className="app-container">Developer portfolio {test}</div>;
+  return <div className="app-container">Developer portfolio Test</div>;
 }
 
 export default App;
