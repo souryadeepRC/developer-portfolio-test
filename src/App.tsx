02a3778b869dbfd861db5f245b103d0349780017
Souryadeep Roy Chowdhury <84499348+souryadeepRC@users.noreply.github.com>
2024-08-17 15:51:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 
 function App() {
-  return <div className="app-container">Developer portfolio</div>;
+  return <div className="app-container">Developer portfolio Test</div>;
 }
 
 export default App;
