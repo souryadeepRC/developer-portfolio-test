ae857a91b2f7d8588ae5ce15a4f17c406ecb7a7a
souryadeepRC <thecreativeworldforall@gmail.com>
2024-08-17 14:37:04 +0530

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.scss";
 
 function App() {
   const test = "Hello world";
-  return <div className="app-container">Developer portfolio</div>;
+  return <div className="app-container">Developer portfolio NEW</div>;
 }
 
 export default App;
